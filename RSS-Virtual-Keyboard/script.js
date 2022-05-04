09d59505605146b41ec2837f2db7bec2f8a211a7
kettl96 <kettl110e4@gmail.com>
2022-05-04 22:50:54 +0300

    feat: add switch lang

--- RSS-Virtual-Keyboard/script.js
+++ RSS-Virtual-Keyboard/script.js
@@ -17,6 +17,7 @@ const keyLayoutRu = [
 const endLineArray = ["backspace", "DEL", "enter", "&uarr;"]
 const overSizeBtn = ["Ctrl", "Tab", "Caps", "&mdash;", "backspace", "enter", "Shift", "&uarr;", "Win", "Alt", "&mdash;"]
 
+let langFlag = true
 let sizeBtnCount = 0
 function addElement() {
   let wrapper = document.createElement('div')
@@ -35,7 +36,7 @@ function addElement() {
   textInfo.innerHTML = 'Клавиатура создана в операционной системе Windows  Для переключения языка комбинация: Shift + Alt'
 
   let langStore = localStorage.getItem('lang')
-  langStore == 'ru' ? createButton(keyLayoutRu) : createButton(keyLayout)
+  langStore == 'ru' ? `${createButton(keyLayoutRu), langFlag = false}` : `${createButton(keyLayout), langFlag = true}`
 
   const endLine = document.querySelectorAll('.end')
   endLine.forEach(e => e.after(document.createElement('br')))
@@ -65,6 +66,7 @@ function addElement() {
 
   // keyboard push btn
   const wideBtn = {
+    'Tab': 'Tab',
     'Backspace': "backspace",
     'Delete': 'DEL',
     'CapsLock': 'Caps',
@@ -75,23 +77,35 @@ function addElement() {
     'ArrowUp': "&uarr;",
     'ArrowDown': "&darr;",
     'ArrowLeft': "&larr;",
-    'ArrowRight': "&rarr;"
+    'ArrowRight': "&rarr;",
+    'Shift' : 'Shift',
+    'Alt': 'Alt'
   }
 
   function whatButton(btn) {
     let low = btn.toLowerCase()
+    let index
     for (let k in wideBtn) {
-      if (btn === k) pushButton(wideBtn[k])
+      if (btn === k) {
+        index = keyLayout.indexOf(wideBtn[k])
+        return pushButton(wideBtn[k], index)
+      }
     }
     if (keyLayout.includes(low)) {
-      pushButton(low)
+      index = keyLayout.indexOf(low)
+      return pushButton(low, index)
+    }
+    if (keyLayoutRu.includes(low)) {
+      index = keyLayoutRu.indexOf(low)
+      return pushButton(low, index)
     }
   }
-  function pushButton(btn) {
-    switchCase(btn)
-    let indexKey = keyLayout.indexOf(btn)
+  function pushButton(btn, indexKey) {
+    // let indexKey = keyLayout.indexOf(btn)
+    // indexKey == undefined ? indexKey = keyLayoutRu.indexOf(btn) :  indexKey = keyLayout.indexOf(btn)
     let keys = document.querySelectorAll('.key')
     keys[indexKey].classList.add('push')
+    switchCase(keys[indexKey].outerText)
   }
   document.addEventListener('keydown', (e) => whatButton(e.key))
 
@@ -103,7 +117,7 @@ function addElement() {
   // switch
   let capsFlag = false
   function switchCase(condition) {
-    
+    console.log(condition);
     switch (condition) {
       case "backspace":
         // textArea.focus();
@@ -123,17 +137,86 @@ function addElement() {
         textArea.value += "\n"
         break
       case "Shift":
-        textArea.value = ''
+        textArea.value += ''
         break
       case "Alt":
-        textArea.value = ''
+        textArea.value += ''
         break
       case "Ctrl":
-        textArea.value = ''
+        textArea.value += ''
         break
 
-      default: 
-      if (capsFlag) textArea.value += condition.toUpperCase()
-      else textArea.value += condition
+      default:
+        if (capsFlag) textArea.value += condition.toUpperCase()
+        else textArea.value += condition
     }
   }
+  function changeLang() {
+    textArea.value += ''
+    
+    keysList.forEach((e, i) => {
+      langFlag ? e.innerHTML = keyLayoutRu[i] : e.innerHTML = keyLayout[i]
+    })
+    langFlag ? `${langFlag = false, localStorage.setItem('lang', 'ru')}` : `${langFlag = true, localStorage.setItem('lang', 'eng')}`
+    
+  }
+
+
+  const keysList = document.querySelectorAll('.key')
+  function caps() {
+    keysList.forEach(e => {
+      if (e.outerText.length == 1 && `${!capsFlag ? e.outerText !== e.outerText.toUpperCase() : e.outerText !== e.outerText.toLowerCase()}`) {
+        !capsFlag
+          ? e.innerHTML = e.outerText.toUpperCase()
+          : e.innerHTML = e.outerText.toLowerCase()
+      }
+    })
+    !capsFlag ? capsFlag = true : capsFlag = false
+  }
+
+  // double push
+  function twoKeysDown(func, ...codes) {
+    let pressed = new Set()
+
+    document.addEventListener('keydown', function (event) {
+      pressed.add(event.code)
+      for (let code of codes) {
+        if (!pressed.has(code)) return
+      }
+      pressed.clear()
+      func()
+    })
+    document.addEventListener('keyup', function (event) {
+      pressed.delete(event.code)
+    })
+  }
+  twoKeysDown(() => changeLang(), 'ShiftLeft', 'AltLeft')
+
+
+  function backspace() {
+    textArea.se
+    var ss = textArea.selectionStart;
+    var se = textArea.selectionEnd;
+    var ln = textArea.value.length;
+
+    var textbefore = textArea.value.substring(0, ss)
+    var textselected = textArea.value.substring(ss, se)
+    var textafter = textArea.value.substring(se, ln)
+
+    if (ss == se) {
+      textArea.value = textArea.value.substring(0, textArea.value.length - 1)
+      textArea.focus();
+      textArea.selectionStart = ss - 1;
+      textArea.selectionEnd = ss - 1;
+    } else {
+      textArea.value = textbefore + textafter;
+      textArea.focus();
+      textArea.selectionStart = ss;
+      textArea.selectionEnd = ss;
+    }
+
+  }
+  // document.addEventListener('keydown', (e) => textArea.value='')
+
+
+}
